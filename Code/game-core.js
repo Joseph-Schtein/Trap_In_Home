c8e5e6b54d376f0499dee1e1450134db66ab45d9
Rina Oksman <rinaok@gmail.com>
2026-06-06 14:56:10 +0300

    add digits to the inventory

--- Code/game-core.js
+++ Code/game-core.js
@@ -389,7 +389,7 @@ function triggerPhoneDialogue() {
         showText("Spouse via Text", `Wow, are you a bear hibernating? I landed. Here's one of the digits: ${targetCode[0]}. Find the rest in the other rooms!`);
     }
     if (!gameState.inventory.includes(`Note: ${targetCode[0]}`)) {
-        addItem(`Note: ${targetCode[0]}`);
+        addItem(`${targetCode[0]}`);
     }
 }
 
